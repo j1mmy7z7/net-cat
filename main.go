31c3d7233126652ab91618740a803f44c81a11ab
wnjuguna <wm453856@gmail.com>
2024-10-30 10:19:13 +0300

    feat: Add main function for the server

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	net "netcat/connections"
+
+)
+
+
+
+func main() {
+	defaultPort := "8989"
+
+	port := defaultPort
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	}
+
+	if _, err := strconv.Atoi(port); err != nil {
+		fmt.Printf("Invalid port number: %v", err)
+	}
+
+
+	log.Printf("Starting server on port %s...", port)
+	server := net.NewServer(":" + port)
+	server.Start()
+}
