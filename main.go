3c85f8d67930d7d04b8ca375276b2f0f6871642e
jmuchiri <mmjames451@gmail.com>
2024-10-31 11:11:30 +0300

    fix: add check for invalid arguments

--- main.go
+++ main.go
@@ -10,15 +10,29 @@ import (
 )
 
 func main() {
-	defaultPort := "8989"
+	var port string
 
-	port := defaultPort
-	if len(os.Args) > 1 {
-		port = os.Args[1]
+
+	Args := os.Args[1:]
+
+	if len(Args) == 0 {
+		port = "8989"
+	} else if len(Args) == 1 {
+		port = Args[0]
+	} else {
+		fmt.Println("[USAGE]: ./TCPChat $port")
+		return
 	}
 
-	if _, err := strconv.Atoi(port); err != nil {
+	checker , err := strconv.Atoi(port); 
+	if err != nil {
 		fmt.Printf("Invalid port number: %v", err)
+		return
+	}
+	
+	if checker < 0 || checker > 65535 {
+		fmt.Println("Enter a valid port number between 0 - 65535")
+		return
 	}
 
 
